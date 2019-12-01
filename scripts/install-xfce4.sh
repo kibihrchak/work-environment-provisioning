ea626695d0b8a1b49d9931f8d3acc818e90aed1d
Marko Oklobdžija <marko.n.oklobdzija@gmail.com>
2019-12-01 18:46:33 +0100

    Update provision to install Xfce 4

---
+++ scripts/install-xfce4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -eux
+
+echo "==> Update package info"
+apt update
+
+echo "==> Installing XFCE4 prerequisites"
+sudo apt install -y xorg xinit dbus-x11 upower --no-install-recommends
+
+echo "==> Installing XFCE4 and additional stuff"
+sudo apt install -y xfce4 xfce4-taskmanager xfce4-systemload-plugin xfce4-whiskermenu-plugin --no-install-recommends
+
+#   [TODO] Copy theme, config files
+#   [TODO] Configure bashrc so that xfce4 is started on login
