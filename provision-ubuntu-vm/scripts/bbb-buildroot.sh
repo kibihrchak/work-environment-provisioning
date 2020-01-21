30483a9edf396c70a33b8c7ea9d9835a8eb86d72
Marko Oklobdžija <marko.n.oklobdzija@gmail.com>
2020-01-21 19:31:41 +0100

    [WIP] Add RPi-based files

--- provision-ubuntu-vm/scripts/bbb-buildroot.sh
+++ provision-ubuntu-vm/scripts/bbb-buildroot.sh
@@ -2,21 +2,58 @@
 
 SSH_USER=${SSH_USERNAME:-vagrant}
 SSH_GROUP=$(id -g ${SSH_USER})
-
-echo "==> Installing network configuration (netplan)"
-#   [TODO]
+BUILDROOT_BASE_NAME="buildroot-${BUILDROOT_VERSION}"
+BUILDROOT_ARCHIVE_NAME="${BUILDROOT_BASE_NAME}.tar.gz"
 
 echo "==> Installing file servers (NFS, TFTP)"
-apt install tftpd nfs-kernel-server --no-install-recommends
+sudo apt install -y tftpd-hpa nfs-kernel-server --no-install-recommends
 
-echo "==> Installing Buildroot"
-#   [TODO]
+echo "==> Installing Buildroot-needed packages"
+sudo apt install -y \
+    sed make binutils gcc g++ bash patch libncurses-dev \
+    gzip bzip2 perl tar cpio unzip rsync wget bc \
+    --no-install-recommends
 
-echo "==> Copying configuration"
-#   [TODO]
+echo "==> Installing serial console, configuring user"
+sudo apt install -y picocom --no-install-recommends
+sudo usermod -a -G dialout ${SSH_USER}
 
-echo "==> Executing build"
-#   [TODO]
+echo "==> Configuring system"
+sudo cp -R /tmp/files/config/bbb-buildroot/root/. /
+sudo mkdir -p /nfs/bbb /tftp/bbb
+sudo mkdir -p /mnt/bbb/boot /mnt/bbb/rfs
+
+echo "==> Configuring Buildroot directory"
+mkdir -p ~/buildroot/build
+cp -R /tmp/files/config/bbb-buildroot/buildroot/. \
+    ~/buildroot/
+chmod 755 ~/buildroot/*.sh
+
+echo "==> Getting Buildroot"
+if [ -z "${BUILDROOT_ARCHIVE_PATH}" ]
+then
+    cd /tmp
+    wget -c \
+        "http://buildroot.org/downloads/${BUILDROOT_ARCHIVE_NAME}" \
+        "http://buildroot.org/downloads/${BUILDROOT_ARCHIVE_NAME}.sign"
+    grep MD5 "${BUILDROOT_ARCHIVE_NAME}.sign" | cut -d' ' -f2- | md5sum -c
+    tar -ax \
+        --owner "${SSH_USER}" \
+        -f "${BUILDROOT_ARCHIVE_NAME}" \
+        -C ~/buildroot/
+    rm "${BUILDROOT_ARCHIVE_NAME}"
+
+    echo "==> Configuring Buildroot repo"
+    export BR2_DEFCONFIG=~/buildroot/defconfig
+    cd ~/buildroot/build
+    make -C ../${BUILDROOT_BASE_NAME} O="$(pwd)" defconfig
+
+    echo "==> Executing build"
+    ~/buildroot/build.sh
+else
+    tar -xzf "${BUILDROOT_ARCHIVE_PATH}" -C ~/buildroot \
+        --checkpoint 1000
+fi
 
 echo "==> Deploying build to file servers"
-#   [TODO]
+~/buildroot/deploy.sh
