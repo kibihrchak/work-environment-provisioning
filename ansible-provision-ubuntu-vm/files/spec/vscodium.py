2aaa77cabc40b551e5c5b21a08fa0041240f3b79
Marko Oklobdžija <marko.n.oklobdzija@gmail.com>
2020-08-06 00:17:57 +0200

    Add VSCodium provisioning

---
+++ ansible-provision-ubuntu-vm/files/spec/vscodium.py
@@ -0,0 +1,27 @@
+import testinfra
+
+def test_vscodium_install(host):
+    assert host.package("codium").is_installed
+
+def test_vscodium_config_copy(host):
+    assert not host.ansible(
+        "copy",
+        "src=/tmp/files/config/vscodium/ dest=$HOME")["changed"]
+
+def test_vscodium_extensions(host):
+    expected_extensions = (
+          "alefragnani.Bookmarks",
+          "ms-vscode.cpptools",
+          "platformio.platformio-ide",
+          "stkb.rewrap",
+          "streetsidesoftware.code-spell-checker",
+          "vscodevim.vim",
+          "ms-vscode.cmake-tools",
+          "twxs.cmake"
+          )
+
+    cmd = host.run("codium %s", "--list-extensions")
+    installed_extensions = cmd.stdout.splitlines()
+
+    for expected_extension in expected_extensions:
+        assert expected_extension in installed_extensions
