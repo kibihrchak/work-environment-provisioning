6f86438e832f687d19330714e8cfc90612945ba6
Marko Oklobdžija <marko.n.oklobdzija@gmail.com>
2020-08-07 00:22:54 +0200

    Remove templates duplicated by Ansible

--- provision-ubuntu-vm/scripts/install-cmake.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eux
-
-SSH_USER=${SSH_USERNAME:-vagrant}
-SSH_GROUP=$(id -g ${SSH_USER})
-
-echo "==> Installing needed development tools"
-sudo DEBIAN_FRONTEND=noninteractive \
-    apt install -y --no-install-recommends \
-    cmake clang-tidy clang-format
