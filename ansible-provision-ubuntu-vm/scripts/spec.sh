1963bdee0f781d5e1f844d62d40aab7ea7fbb18e
Marko Oklobdžija <marko.n.oklobdzija@gmail.com>
2020-10-16 18:37:35 +0000

    Change from "specs" to "spec"
    Dir is named "spec".

--- ansible-provision-ubuntu-vm/scripts/spec.sh
+++ ansible-provision-ubuntu-vm/scripts/spec.sh
@@ -3,12 +3,12 @@
 SSH_USER=${SSH_USERNAME:-vagrant}
 SSH_GROUP=$(id -g ${SSH_USER})
 SPECS=${ANSIBLE_PLAYBOOKS}
-SPECS_DIR_DEFAULT="$(dirname "$0")/../files/spec"
-SPECS_DIR="${ANSIBLE_SPEC_DIR:-${SPECS_DIR_DEFAULT}}"
+SPEC_DIR_DEFAULT="$(dirname "$0")/../files/spec"
+SPEC_DIR="${ANSIBLE_SPEC_DIR:-${SPECS_DIR_DEFAULT}}"
 
 echo "==> Running specs for all stated playbooks"
 IFS=","
 for spec in $SPECS
 do
-    pytest --hosts="ansible://devvm" "${SPECS_DIR}/${spec}.py"
+    pytest --hosts="ansible://devvm" "${SPEC_DIR}/${spec}.py"
 done
