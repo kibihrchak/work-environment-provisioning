992723864f7ae2b68ca7916a46b80406b849dfc5
Marko Oklobdžija <marko.n.oklobdzija@gmail.com>
2020-10-16 18:36:32 +0000

    Fix passing of spec files dir path
    This caused an issue when calling specs from Packer.

--- ansible-provision-ubuntu-vm/scripts/spec.sh
+++ ansible-provision-ubuntu-vm/scripts/spec.sh
@@ -4,7 +4,7 @@ SSH_USER=${SSH_USERNAME:-vagrant}
 SSH_GROUP=$(id -g ${SSH_USER})
 SPECS=${ANSIBLE_PLAYBOOKS}
 SPECS_DIR_DEFAULT="$(dirname "$0")/../files/spec"
-SPECS_DIR="${ANSIBLE_SPECS_DIR:-${SPECS_DIR_DEFAULT}}"
+SPECS_DIR="${ANSIBLE_SPEC_DIR:-${SPECS_DIR_DEFAULT}}"
 
 echo "==> Running specs for all stated playbooks"
 IFS=","
