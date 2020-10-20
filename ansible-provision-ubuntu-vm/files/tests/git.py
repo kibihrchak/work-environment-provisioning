19c780a16095d7f50fcadbef88e40892c53e2cee
Marko Oklobdžija <marko.n.oklobdzija@gmail.com>
2020-10-20 22:54:57 +0000

    Check user/email for Git provisioning test

--- ansible-provision-ubuntu-vm/files/tests/git.py
+++ ansible-provision-ubuntu-vm/files/tests/git.py
@@ -7,7 +7,17 @@ def test_git_support_install(host):
     assert host.package("gitk").is_installed
     assert host.package("git-gui").is_installed
 
-def test_git_config_set_up(host):
+def test_git_config_exists(host):
     import os
-    #   [TODO] Introduce content check
     assert host.file(os.path.join(host.user().home, ".gitconfig")).exists
+
+def test_git_config_expected_content(host):
+    import configparser
+    import os
+
+    config = configparser.ConfigParser()
+    config.read(os.path.join(host.user().home, ".gitconfig"))
+    assert config.has_option("user", "name")
+    assert config["user"]["name"] == os.environ["GIT_USER_NAME"]
+    assert config.has_option("user", "email")
+    assert config["user"]["email"] == os.environ["GIT_USER_EMAIL"]
